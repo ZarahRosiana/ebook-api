a6d6877be3a4ec60fbe1aab66dc6da64335126bb
ZarahRosiana <zrosiana@gmail.com>
2020-10-08 21:17:08 +0700

    auth

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Author;
 
+use JWTAuth;
+
 class AuthorController extends Controller
 {
     /**
@@ -14,23 +16,18 @@ class AuthorController extends Controller
      */
     public function index()
     {
-        $authors = Author::all();
-        if ($authors && $authors->count() > 0) {
-            return response(['message' => 'Show data successful', 'data' => $authors], 200);
- 
-        }
-        return response(['message' => 'No data to be displayed', 'data' => null], 404);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-
         //
+        $author = Author::all();
+        if($author && $author->count() > 0){
+            return response(['message'=> 'Show data success.', 'data'=> $author], 200);
+        }else{
+            return response(['message'=> 'Data not found.', 'data'=> null], 404);
+        }
+    }
+        public function __construct() {
+            $this->middleware('auth:api');
+
+        
     }
 
     /**
@@ -41,17 +38,16 @@ class AuthorController extends Controller
      */
     public function store(Request $request)
     {
-        // id, name, date_of_birth, place_of_birth, gender, email, hp , create_at, update_at.
-        $data = Author::create([
-            "name" => $request->input('name'),
-            "date_of_birth" => $request->input('date_of_birth'),
-            "place_of_birth" => $request->input('place_of_birth'),
-            "gender" => $request->input('gender'),
-            "email" => $request->input('email'),
-            "hp" => $request->input('hp'),
-        ]);
-
-        return response(['message' => 'Create data success', 'data' => $data], 201);
+        //
+        $author = Author::create ([
+            "name" => $request->name,
+            "date_of_birth" => $request->date_of_birth,
+            "place_of_birth" => $request->place_of_birth,
+            "gender" => $request->gender,
+            "email" => $request->email,
+            "hp" => $request->hp
+     ]);
+        return response(['message'=> 'Create data success.', 'data'=> $author], 201);    
     }
 
     /**
@@ -64,21 +60,11 @@ class AuthorController extends Controller
     {
         //
         $author = Author::find($id);
-        if ($author) {
-            return response(['message' => 'Show data success', 'data' => $author], 200);
+        if($author && $author->count() > 0){
+            return response(['message'=> 'Show data success.', 'data'=> $author], 200);
+        }else{
+            return response(['message'=> 'Data not found.', 'data'=> null], 404);
         }
-        return response(['message' => 'Author not found', 'data' => null], 404);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
     }
 
     /**
@@ -92,19 +78,18 @@ class AuthorController extends Controller
     {
         //
         $author = Author::find($id);
-        if ($author) {
-            $data = $author->update([
-                "name" => $request->input('name'),
-                "date_of_birth" => $request->input('date_of_birth'),
-                "place_of_birth" => $request->input('place_of_birth'),
-                "gender" => $request->input('gender'),
-                "email" => $request->input('email'),
-                "hp" => $request->input('hp'),
-            ]);
-            return response(['message' => 'Author updated succesfully.', 'data' => $data], 201);
+        if($author){
+            $author ->name = $request->name;
+            $author ->date_of_birth = $request->date_of_birth;
+            $author ->place_of_birth = $request->place_of_birth;
+            $author ->gender = $request->gender;
+            $author ->email = $request->email;
+            $author ->hp = $request->hp;
+            $author->save();
+            return response(['message'=> 'Update data success.', 'data'=> $author], 200);
+        }else{
+            return response(['message'=> 'Update data failed.', 'data'=> null], 406);
         }
-
-        return response(['message' => 'no author to update', 'data' => null], 404);
     }
 
     /**
@@ -115,13 +100,13 @@ class AuthorController extends Controller
      */
     public function destroy($id)
     {
-        //
         $author = Author::find($id);
-        if ($author) {
-            $data = Author::destroy($id);
-            return response(['message' => 'author deleted succesfully', 'data' => $data], 201);
+        if($author){
+            $author->delete();
+            return response([], 204);
+        }else{
+            return response(['message'=> 'Remove data failed.', 'data'=> null], 406);
+            
         }
-
-        return response(['message' => 'no author to delete', 'data' => null], 404);
-    }
+    }    
 }
--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Book;
 
+use JWTAuth;
+
 class BookController extends Controller
 {
     /**
@@ -14,23 +16,13 @@ class BookController extends Controller
      */
     public function index()
     {
-        $book = Book::all();
-        if ($book && $book->count() > 0){
-            return response(['message' => 'Show data success.', 'data'=> $book], 200);
-        }else{
-            return response(['message' => 'Data not found.', 'data'=> null], 404);
-        }
+        //
+        return Book::get();
+    }
+    public function __construct() {
+        $this->middleware('auth:api');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -40,16 +32,17 @@ class BookController extends Controller
      */
     public function store(Request $request)
     {
+        //
         return Book::create([
-            "title" => $request->input('title'),
-            "description" => $request->input('description'),
-            "author" => $request->input('author'),
-            "publisher" => $request->input('publisher'),
-            "date_of_issue" => $request->input('date_of_issue'),
+            "title" => $request->title,
+            "description" => $request->description,
+            "author" => $request->author,
+            "publisher" => $request->publisher,
+            "date_of_issue" => $request->date_of_issue
         ]);
     }
 
-     /**
+    /**
      * Display the specified resource.
      *
      * @param  int  $id
@@ -58,18 +51,10 @@ class BookController extends Controller
     public function show($id)
     {
         //
+        return Book::find($id);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+   
 
     /**
      * Update the specified resource in storage.
@@ -81,15 +66,19 @@ class BookController extends Controller
     public function update(Request $request, $id)
     {
         //
-        return Book::find($id)->update([
-            "title" => $request->input('title'),
-            "description" => $request->input('description'),
-            "author" => $request->input('author'),
-            "publisher" => $request->input('publisher'),
-            "date_of_issue" => $request->input('date_of_issue'),
-        ]);
+        $book = Book::find($id);
+        if($book){
+            $book ->title = $request->title;
+            $book ->description = $request->description;
+            $book ->author = $request->author;
+            $book ->publisher = $request->publisher;
+            $book ->date_of_issue = $request->date_of_issue;
+            $book->save();
+            return response(['message'=> 'Update data success.', 'data'=> $book], 200);
+        }else{
+            return response(['message'=> 'Update data failed.', 'data'=> null], 406);
+        }
     }
-
     /**
      * Remove the specified resource from storage.
      *
@@ -98,6 +87,13 @@ class BookController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $book = Book::find($id);
+        if($book){
+            $book->delete();
+            return response([], 204);
+        }else{
+            return response(['message'=> 'Remove data failed.', 'data'=> null], 406);
+            
+        }
     }
 }
